7872f48972d0760dffa5da5162700198afd4294b
OniiCoder <oniicoder@gmail.com>
2022-11-14 13:31:57 +0000

    Backend routes

--- routes/backend.php
+++ routes/backend.php
@@ -1,3 +1,19 @@
 <?php
+use Astrogoat\GoLoadUp\Http\Controllers\GoLoadUpController;
+use Illuminate\Support\Facades\Route;
 
-// Here you can add routes for the backend.
+Route::group([
+    'as' => 'go-load-up.',
+    'prefix' => 'go-load-up/'
+], function () {
+    Route::get('/', [GoLoadUpController::class, 'index'])->name('index');
+
+    Route::group([
+        'as' => 'zip-codes.',
+        'prefix' => 'zip-codes/'
+    ], function () {
+        Route::get('/{zipCode}/edit', [GoLoadUpController::class, 'edit'])->name('edit');
+        Route::get('/upload', \Astrogoat\GoLoadUp\Http\Livewire\Upload\CsvUploadForm::class)->name('upload');
+    });
+});
+
