b9e7a8e4ff86031441495ed96e66303318c0c3eb
OniiCoder <oniicoder@gmail.com>
2023-03-08 14:55:51 +0000

    Add key to settings migration

--- database/migrations/settings/2021_09_30_100000_create_go-load-up_settings.php
+++ database/migrations/settings/2021_09_30_100000_create_go-load-up_settings.php
@@ -7,6 +7,7 @@ class CreateGoLoadUpSettings extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('go-load-up.enabled', false);
+         $this->migrator->add('go-load-up.white_glove_shopify_product_ID', '');
         // $this->migrator->add('go-load-up.url', '');
         // $this->migrator->addEncrypted('go-load-up.access_token', '');
     }
@@ -14,6 +15,7 @@ class CreateGoLoadUpSettings extends SettingsMigration
     public function down()
     {
         $this->migrator->delete('go-load-up.enabled');
+         $this->migrator->delete('go-load-up.white_glove_shopify_product_ID');
         // $this->migrator->delete('go-load-up.url');
         // $this->migrator->delete('go-load-up.access_token');
     }
