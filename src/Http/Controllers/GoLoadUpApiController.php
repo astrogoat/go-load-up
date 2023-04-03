b405a4255e89ad2d4e753e3d0ca3b96a2bcf4454
OniiCoder <oniicoder@gmail.com>
2023-04-03 12:53:55 +0000

    Feature: zip codes can now be verified for eligibility and mismatched variants are flagged

---
+++ src/Http/Controllers/GoLoadUpApiController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Astrogoat\GoLoadUp\Http\Controllers;
+
+use Astrogoat\GoLoadUp\GoLoadUp;
+use Astrogoat\GoLoadUp\Settings\GoLoadUpSettings;
+use Astrogoat\Shopify\Models\Product;
+use Astrogoat\Shopify\Models\ProductVariant;
+use Illuminate\Http\Request;
+
+class GoLoadUpApiController
+{
+
+    public function checkEligibility(Request $request)
+    {
+        $wgLineItems = $request->wg_line_items;
+        $zipCode = $request->zip;
+
+        $validZipCode = GoLoadUp::verifyZipCode($zipCode);
+
+        if(is_null($validZipCode)) return response()->json([
+                'status' => 404,
+                'error_message' => "Not in service area."
+            ]);
+
+        if ($validZipCode->is_california) {
+            $shopifyId = $this->getRemovalOnlyMattress(false)?->shopify_id;
+        } else {
+            $shopifyId = $this->getRemovalOnlyMattress(true)?->shopify_id;
+        }
+
+        if(in_array($shopifyId, $wgLineItems)) return response()->json([
+            'status' => 406,
+            'wg_line_item' => $shopifyId,
+            'error_message' => "OMR and Zip mismatch"
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => "All good!"
+        ]);
+
+    }
+
+    public function getRemovalOnlyMattress($isCA = false)
+    {
+        $key = $isCA ? "CA_removal_only_mattress_shopify_ID" : "removal_only_mattress_shopify_ID";
+        return ProductVariant::where('shopify_id', settings(GoLoadUpSettings::class, $key))?->first();
+    }
+
+}
