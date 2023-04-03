6aee3d06037b88343334daa4257bb1f6d5b25d5a
OniiCoder <oniicoder@gmail.com>
2023-04-03 12:52:34 +0000

    Display drop down to select products from settings

--- src/Settings/GoLoadUpSettings.php
+++ src/Settings/GoLoadUpSettings.php
@@ -4,16 +4,37 @@ namespace Astrogoat\GoLoadUp\Settings;
 
 use Helix\Lego\Settings\AppSettings;
 use Illuminate\Validation\Rule;
+use Astrogoat\Shopify\Models\Product;
+
 
 class GoLoadUpSettings extends AppSettings
 {
     public string $white_glove_shopify_product_ID;
+    public string $CA_removal_only_mattress_shopify_ID;
+    public string $removal_only_mattress_shopify_ID;
 
     public function rules(): array
     {
         return [
              'white_glove_shopify_product_ID' => Rule::requiredIf($this->enabled === true),
+             'CA_removal_only_mattress_shopify_ID' => Rule::requiredIf($this->enabled === true),
+             'removal_only_mattress_shopify_ID' => Rule::requiredIf($this->enabled === true),
         ];
+    }
+
+    public function whiteGloveShopifyProductIdOptions()
+    {
+        return Product::orderBy('title', 'asc')->get()->pluck('title', 'shopify_id')->toArray();
+    }
+
+    public function caRemovalOnlyMattressShopifyIdOptions()
+    {
+        return Product::where('shopify_id', $this->white_glove_shopify_product_ID)->first()?->variants()->pluck('title', 'shopify_id')->toArray() ?? [];
+    }
+
+    public function removalOnlyMattressShopifyIdOptions()
+    {
+        return Product::where('shopify_id', $this->white_glove_shopify_product_ID)->first()?->variants()->pluck('title', 'shopify_id')->toArray() ?? [];
     }
 
     public function description(): string
