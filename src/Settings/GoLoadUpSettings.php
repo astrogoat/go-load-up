a0938454f444fdb441cd3555631245c1731cc1db
Kristoffer Tonning <tonning@gmail.com>
2023-04-04 15:28:29 +0200

    Add labels and help to settings fields

--- src/Settings/GoLoadUpSettings.php
+++ src/Settings/GoLoadUpSettings.php
@@ -9,15 +9,15 @@ use Illuminate\Validation\Rule;
 class GoLoadUpSettings extends AppSettings
 {
     public string $white_glove_shopify_product_id;
-    public string $california_removal_only_mattress_shopify_id;
     public string $removal_only_mattress_shopify_id;
+    public string $california_removal_only_mattress_shopify_id;
 
     public function rules(): array
     {
         return [
              'white_glove_shopify_product_id' => Rule::requiredIf($this->enabled === true),
-             'california_removal_only_mattress_shopify_id' => Rule::requiredIf($this->enabled === true),
              'removal_only_mattress_shopify_id' => Rule::requiredIf($this->enabled === true),
+             'california_removal_only_mattress_shopify_id' => Rule::requiredIf($this->enabled === true),
         ];
     }
 
@@ -26,7 +26,7 @@ class GoLoadUpSettings extends AppSettings
         return Product::orderBy('title', 'asc')->get()->pluck('title', 'shopify_id')->toArray();
     }
 
-    public function caRemovalOnlyMattressShopifyIdOptions()
+    public function californiaRemovalOnlyMattressShopifyIdOptions()
     {
         return Product::where('shopify_id', $this->white_glove_shopify_product_id)->first()?->variants()->pluck('title', 'shopify_id')->toArray() ?? [];
     }
@@ -36,9 +36,26 @@ class GoLoadUpSettings extends AppSettings
         return Product::where('shopify_id', $this->white_glove_shopify_product_id)->first()?->variants()->pluck('title', 'shopify_id')->toArray() ?? [];
     }
 
+    public function labels(): array
+    {
+        return [
+            'white_glove_shopify_product_id' => 'White Glove product',
+            'california_removal_only_mattress_shopify_id' => 'Mattress only removal product variant for California',
+            'removal_only_mattress_shopify_id' => 'Mattress only removal only product variant',
+        ];
+    }
+
+    public function help(): array
+    {
+        return [
+            'california_removal_only_mattress_shopify_id' => 'Select the White Glove product above first.',
+            'removal_only_mattress_shopify_id' => 'Select the White Glove product above first.',
+        ];
+    }
+
     public function description(): string
     {
-        return 'Interact with GoLoadUp.';
+        return 'Junk removal. The modern way.';
     }
 
     public static function group(): string
