4ce96764920717c612fd90f8e9a846b5189cfa59
OniiCoder <oniicoder@gmail.com>
2023-06-22 10:34:25 +0000

    wip

--- database/migrations/2022_10_31_000001_create_go_load_up_serviceable_zip_codes_table.php
+++ database/migrations/2022_10_31_000001_create_go_load_up_serviceable_zip_codes_table.php
@@ -8,14 +8,16 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('go_load_up_serviceable_zip_codes', function (Blueprint $table) {
-            $table->id();
-            $table->string('zip')->unique();
-            $table->string('name');
-            $table->boolean('is_california')->default(false);
-            $table->boolean('is_eligible')->default(true);
-            $table->timestamps();
-        });
+        if(!Schema::hasTable('go_load_up_serviceable_zip_codes')) {
+            Schema::create('go_load_up_serviceable_zip_codes', function (Blueprint $table) {
+                $table->id();
+                $table->string('zip')->unique();
+                $table->string('name');
+                $table->boolean('is_california')->default(false);
+                $table->boolean('is_eligible')->default(true);
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
--- database/migrations/settings/2021_09_30_100000_create_go_load_up_settings.php
+++ database/migrations/settings/2021_09_30_100000_create_go_load_up_settings.php
@@ -6,10 +6,12 @@ class CreateGoLoadUpSettings extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('go-load-up.enabled', false);
-        $this->migrator->add('go-load-up.white_glove_shopify_product_id', '');
-        $this->migrator->add('go-load-up.california_removal_only_mattress_shopify_id', '');
-        $this->migrator->add('go-load-up.removal_only_mattress_shopify_id', '');
+        if($this->migrator->has('go-load-up.enabled')) {
+            $this->migrator->add('go-load-up.enabled', false);
+            $this->migrator->add('go-load-up.white_glove_shopify_product_id', '');
+            $this->migrator->add('go-load-up.california_removal_only_mattress_shopify_id', '');
+            $this->migrator->add('go-load-up.removal_only_mattress_shopify_id', '');
+        }
     }
 
     public function down()
