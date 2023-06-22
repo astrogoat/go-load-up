340393a35fe714066a50679c842a7ad46c08fff6
OniiCoder <oniicoder@gmail.com>
2023-06-22 10:41:12 +0000

    wip

--- database/migrations/settings/2021_09_30_100000_create_go_load_up_settings.php
+++ database/migrations/settings/2021_09_30_100000_create_go_load_up_settings.php
@@ -6,12 +6,10 @@ class CreateGoLoadUpSettings extends SettingsMigration
 {
     public function up(): void
     {
-        if(!$this->migrator->has('go-load-up.enabled')) {
-            $this->migrator->add('go-load-up.enabled', false);
-            $this->migrator->add('go-load-up.white_glove_shopify_product_id', '');
-            $this->migrator->add('go-load-up.california_removal_only_mattress_shopify_id', '');
-            $this->migrator->add('go-load-up.removal_only_mattress_shopify_id', '');
-        }
+        $this->migrator->add('go-load-up.enabled', false);
+        $this->migrator->add('go-load-up.white_glove_shopify_product_id', '');
+        $this->migrator->add('go-load-up.california_removal_only_mattress_shopify_id', '');
+        $this->migrator->add('go-load-up.removal_only_mattress_shopify_id', '');
     }
 
     public function down()
