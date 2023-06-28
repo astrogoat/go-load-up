30bb58cf8b5396ead10caeb1df4e5eb3d3e2a806
OniiCoder <oniicoder@gmail.com>
2023-06-28 22:28:19 +0000

    Remove check

--- database/migrations/2023_04_11_000008_create_go_load_up_cart_requirements_table.php
+++ database/migrations/2023_04_11_000008_create_go_load_up_cart_requirements_table.php
@@ -8,18 +8,16 @@ return new class extends Migration
 {
     public function up()
     {
-        if(!Schema::hasTable('go_load_up_cart_requirements')) {
-            Schema::create('go_load_up_cart_requirements', function (Blueprint $table) {
-                $table->id();
-                $table->unsignedInteger('service_product_variant_id')->unique();
-                $table->json('first_set_of_required_shopify_product_ids')->nullable();
-                $table->json('second_set_of_required_shopify_product_ids')->nullable();
-                $table->text('error_message');
-                $table->timestamps();
+        Schema::create('go_load_up_cart_requirements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('service_product_variant_id')->unique();
+            $table->json('first_set_of_required_shopify_product_ids')->nullable();
+            $table->json('second_set_of_required_shopify_product_ids')->nullable();
+            $table->text('error_message');
+            $table->timestamps();
 
-                $table->foreign('service_product_variant_id')->references('id')->on('shopify_product_variants')->onDelete('CASCADE');
-            });
-        }
+            $table->foreign('service_product_variant_id')->references('id')->on('shopify_product_variants')->onDelete('CASCADE');
+        });
     }
 
     public function down()
--- database/migrations/2023_04_12_000002_create_go_load_up_services_table.php
+++ database/migrations/2023_04_12_000002_create_go_load_up_services_table.php
@@ -8,14 +8,12 @@ return new class extends Migration
 {
     public function up()
     {
-        if(!Schema::hasTable('go_load_up_services')) {
-            Schema::create('go_load_up_services', function (Blueprint $table) {
-                $table->id();
-                $table->integer('code')->unique();
-                $table->json('product_variant_ids');
-                $table->timestamps();
-            });
-        }
+        Schema::create('go_load_up_services', function (Blueprint $table) {
+            $table->id();
+            $table->integer('code')->unique();
+            $table->json('product_variant_ids');
+            $table->timestamps();
+        });
     }
 
     public function down()
